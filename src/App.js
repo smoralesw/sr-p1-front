579930a182cdd34f8b372c66f11bc296378bae0e
smoralesw <s.moralesw@uc.cl>
2022-10-26 19:21:44 -0300

    searchbar done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [nameIn, setNameIn] = useState('');
+  const [artistIn, setArtistIn] = useState('');
+  const [nameOut, setNameOut] = useState('');
+  const [artistOut, setArtistOut] = useState('');
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Ingresa una canción de entrada</p>
+        <input className="search-bar" type="text" placeholder="Nombre" onChange={(event) => setNameIn(event.target.value)}/>
+        <input className="search-bar" type="text" placeholder="Artista" onChange={(event) => setArtistIn(event.target.value)}/>
+
+        <p>Ingresa una canción de salida</p>
+        <input className="search-bar" type="text" placeholder="Nombre" onChange={(event) => setNameOut(event.target.value)}/>
+        <input className="search-bar" type="text" placeholder="Artista" onChange={(event) => setArtistOut(event.target.value)}/>
+
+        <p>{nameIn}</p>
+        <p>{artistIn}</p>
+        <p>{nameOut}</p>
+        <p>{artistOut}</p>
       </header>
+
     </div>
   );
 }
