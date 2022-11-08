126eb43bb01cac8ba7e7475af9099c3f9e417407
smoralesw <s.moralesw@uc.cl>
2022-11-08 16:03:40 -0300

    front - maqueta lista

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
 import Axios from 'axios';
 import React, { useState } from 'react';
 import PathsInfo from './PathsInfo';
+import './App.css';
 
 function App() {
   const [nameIn, setNameIn] = useState('');
   const [artistIn, setArtistIn] = useState('');
   const [nameOut, setNameOut] = useState('');
   const [artistOut, setArtistOut] = useState('');
-  const [response, setResponse] = useState();
+  const [responseKeys, setResponseKeys] = useState();
+  const [responseValues, setResponseValues] = useState();
 
 
   const handleSend = async () => {
@@ -19,7 +20,8 @@ function App() {
         nameOut: nameOut,
         artistOut: artistOut
       })
-      setResponse(resp.data);
+      setResponseKeys(Object.keys(resp.data.result.most_stable_paths));
+      setResponseValues(Object.values(resp.data.result.most_stable_paths));
     } catch (error) {
       console.log(error);
     }
@@ -28,23 +30,24 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="header">
         <p>Ingresa una canción de entrada</p>
-        <input className="search-bar" type="text" placeholder="Nombre" onChange={(event) => setNameIn(event.target.value)}/>
-        <input className="search-bar" type="text" placeholder="Artista" onChange={(event) => setArtistIn(event.target.value)}/>
+        <input className="search-bar" id="search-bar" type="text" placeholder="Nombre" onChange={(event) => setNameIn(event.target.value)}/>
+        <input className="search-bar" id="search-bar" type="text" placeholder="Artista" onChange={(event) => setArtistIn(event.target.value)}/>
 
         <p>Ingresa una canción de salida</p>
-        <input className="search-bar" type="text" placeholder="Nombre" onChange={(event) => setNameOut(event.target.value)}/>
-        <input className="search-bar" type="text" placeholder="Artista" onChange={(event) => setArtistOut(event.target.value)}/>
+        <input className="search-bar" id="search-bar" type="text" placeholder="Nombre" onChange={(event) => setNameOut(event.target.value)}/>
+        <input className="search-bar" id="search-bar" type="text" placeholder="Artista" onChange={(event) => setArtistOut(event.target.value)}/>
 
         {/* Send Info */}
         <button className="search-button" onClick={() => handleSend() }>Buscar</button>        
+        
+        <div>
+          {/* if response is not null */}
+          {responseKeys && <PathsInfo paths_len={ responseKeys } paths={ responseValues }/>}
+        </div>
       </header>
 
-      <div>
-        {/* if response is not null */}
-        {response && <PathsInfo paths_len={Object.keys(response)} paths={Object.values(response)}/>}
-      </div>
 
     </div>
   );
--- src/PathsInfo.js
+++ src/PathsInfo.js
@@ -1,21 +1,59 @@
-import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import React, { useState } from 'react';
+import { Table } from 'react-bootstrap';
+import { ArrowDown } from 'css.gg'
 
-const PathsInfo = (paths_len, paths) => {
-  const [value, setValue] = useState(2);
-  const [data_, setData_] = useState([1,2,3,4]);
+const PathsInfo = ({paths_len, paths}) => {
+  const [pathIndex, setPath] = useState(0);
+
+  const handlePath = async (index) => {
+    setPath(index);
+  }
 
   return (
-    <div>
-      {paths_len}
-      {paths}
-      {/* {paths_len.map((path, index) => (
-        <div key={index}>
-          <h2>Path {index}</h2>
-          <p>{paths[index]}</p>
-        </div>
-        ))} */}
+    <div className="paths-section">
+      <h2>Select Your Prefered Path Length</h2>
+      <Table className='pathsInfoTable' striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            {paths_len.map((path, index) => (
+                <th className="table-header" key={index} onClick={() => handlePath(index)}>
+                  {path}
+                  {(() => {
+                    if (pathIndex === index) {
+                      return <ArrowDown/>
+                    }
+                    return <> </>;
+                  })()} 
+                </th>
+            ))}
+          </tr>
+        </thead>
+      </Table>
+      <Table className='pathsInfoTable' striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Length</th>
+            <th>Path</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Path {pathIndex}</td>
+            <td>{paths_len[pathIndex]}</td>
+            <td>
+              {paths[pathIndex].map((track, index_) => (
+                <div key={index_}>
+                  <p id="reducedFont">{track}</p>
+                </div>
+              ))}
+            </td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
-  );
+    );
 };
   
 export default PathsInfo;
